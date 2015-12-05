e1591a01795ff978c7f0574406729adb2d079d8d
Alejandro Rodríguez Salamanca <alexrs95@gmail.com>
2015-12-05 19:16:06 +0100

    Change slider

--- final_practice/script/script.js
+++ final_practice/script/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
         $("nav").sticky({topSpacing:0});
+        $('.flexslider').flexslider({
+            animation: "slide",
+            keyboard: true
+        });
 
         var states = new Bloodhound({
           datumTokenizer: Bloodhound.tokenizers.whitespace,
@@ -58,6 +62,7 @@ $(document).ready(function(){
             'Wyoming'
           ]
         });
+
         states.initialize();
         $('.search').typeahead({
           highlight: true
