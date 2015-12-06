2447f8caa5e3c49c428d3fc6ecef0debd58c2635
Hectorrr72 <7hector2@gmail.com>
2015-12-06 17:53:35 +0100

    Arreglada la líada

--- final_practice/script/script.js
+++ final_practice/script/script.js
@@ -3,49 +3,32 @@ $(document).ready(function(){
     $("nav").sticky({topSpacing:0});
 
 
-  $('#calendar').fullCalendar({
-    header: {
-        right: 'prev, next'
-    },
+    $('#calendar').fullCalendar({
+    	header: {
+    			right: 'prev,next'
+    	},
+    	defaultDate: '2015-12-12',
+    	eventLimit: true,
+        theme: true,
+        titleFormat: 'MMM YYYY',
+        firstDay: 1,
+        dayNamesShort:['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        fixedWeekCount: false, // allow "more" link when too many events
+    	events: [
+    		{
+    			title: 'All Day Event',
+              start: moment('2015-12-07').format('YYYY-MM-DD'),
+              rendering:'background'
+    		},
+    		{
+    			title: 'Long Event',
+              start: moment('2015-12-11').format('YYYY-MM-DD'),
+              end: moment('2015-12-13').format('YYYY-MM-DD'),
+              rendering:'background'
+    		},
 
-    columnFormat: {
-                month: 'ddd'
-    },
-
-    prev: 'circle-triangle-w',
-    next: 'circle-triangle-e',
-    titleFormat: 'MMM YY',
-    defaultDate: '2015-12-5',
-    eventLimit: true,
-    weekends: true,
-    firstDay: 1,
-    theme: false,
-    dayNamesShort:['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-    fixedWeekCount: false,
-
-    Object, default:
-    {
-        prev: 'right-single-arrow',
-        next: 'right-single-arrow',
-        prevYear: 'left-double-arrow',
-        nextYear: 'right-double-arrow'
-    },
-
-    events: [
-      {
-        title: 'Long Event',
-        start: '2015-12-07',
-        end: '2015-12-09',
-        rendering: 'background'
-      },
-      {
-        title: 'Dinner',
-        start: '2015-12-11',
-        end: '2015-12-11',
-        rendering: 'background'
-      }
-    ]
-  });
+    	],
+    });
 
         var states = new Bloodhound({
           datumTokenizer: Bloodhound.tokenizers.whitespace,
@@ -109,44 +92,6 @@ $(document).ready(function(){
         },
         {
           source: states
-<<<<<<< HEAD
         });
 
-
 });
-
-
-$(document).ready(function() {
-
-	$('#calendar').fullCalendar({
-        theme: true,
-		header: {
-    			right: 'prev,next'
-		},
-		defaultDate: '2015-12-12',
-		eventLimit: true,
-        titleFormat: 'MMM YYYY',
-        firstDay: 1,
-        dayNamesShort:['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-        fixedWeekCount: false, // allow "more" link when too many events
-		events: [
-			{
-				title: 'All Day Event',
-                start: moment('2015-12-07').format('YYYY-MM-DD'),
-                rendering:'background'
-			},
-			{
-				title: 'Long Event',
-                start: moment('2015-12-11').format('YYYY-MM-DD'),
-                end: moment('2015-12-13').format('YYYY-MM-DD'),
-                rendering:'background'
-			},
-
-		],
-	});
-
-});
-=======
-    });
-});
->>>>>>> 27afc83a7176ee41a81dc0baa13ac4ca88d6433d
