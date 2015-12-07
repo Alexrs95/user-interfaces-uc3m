04614f869e3b1822565fbf6652b0cca198d7230f
Hectorrr72 <7hector2@gmail.com>
2015-12-07 19:50:36 +0100

    Calendar and events everywhere

--- final_practice/script/grades.js
+++ final_practice/script/grades.js
@@ -7,3 +7,33 @@ $('table tr').each(function(){
 
 $('table tr').first().addClass('row-first');
 $('table tr').last().addClass('row-last');
+
+$(function(){
+
+    $('#calendar').fullCalendar({
+    	header: {
+    			right: 'prev,next'
+    	},
+    	defaultDate: '2015-12-12',
+    	eventLimit: true,
+        theme: true,
+        titleFormat: 'MMM YYYY',
+        firstDay: 1,
+        dayNamesShort:['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        fixedWeekCount: false, // allow "more" link when too many events
+    	events: [
+    		{
+    			title: 'All Day Event',
+              start: moment('2015-12-07').format('YYYY-MM-DD'),
+              rendering:'background'
+    		},
+    		{
+    			title: 'Long Event',
+              start: moment('2015-12-11').format('YYYY-MM-DD'),
+              end: moment('2015-12-13').format('YYYY-MM-DD'),
+              rendering:'background'
+    		},
+
+    	]
+    });
+});
